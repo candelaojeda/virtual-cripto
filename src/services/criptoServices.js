c346c71fe15f0b30a331599903e6214573e19fdb
candelaojeda <ojedacandelaco@gmail.com>
2022-07-25 18:39:42 -0300

    added functions

--- src/services/criptoServices.js
+++ src/services/criptoServices.js
@@ -5,4 +5,14 @@ const clientApi = axios.create({
   withCredentials: false,
 });
 
-export default {};
+export default {
+  getCompraVentaCriptomoneda(exchange, coin) {
+    return clienteAPI.get(`/${exchange}/${coin}/ars/0.01`);
+  },
+  getCriptomoneda() {
+    return clienteAPI.get("/cryptomkt/btc/ars/0.21");
+  },
+  getPrecioMoneda(coin) {
+    return clienteAPI.get(`/letsbit/${coin}`);
+  },
+};
--- src/services/transactions.js
+++ src/services/transactions.js
@@ -5,6 +5,20 @@ const clienteAPI = axios.create({
   headers: { "x-apiKey": "60eb09146661365596af552f" },
 });
 
-export default{
-    
-}
+export default {
+  getTransacciones(idUser) {
+    return clienteAPI.get(`/transactions?q={"user_id": "${idUser}"}`);
+  },
+  getTransaccionesById(id) {
+    return clienteAPI.get(`/transactions/${id}`);
+  },
+  newTransaction(sellBuy) {
+    return clienteAPI.post("/transactions", sellBuy);
+  },
+  modifyTransaccion(id, editTransaction) {
+    return clienteAPI.put(`/transactions/${id}`, editTransaction);
+  },
+  deleteTransaccion(id) {
+    return clienteAPI.delete(`/transactions/${id}`);
+  },
+};
