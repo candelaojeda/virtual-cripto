7e703f250f144895cf2a95707feecf78a6026096
candelaojeda <ojedacandelaco@gmail.com>
2022-07-25 19:17:49 -0300

    added mutations

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
-import { createStore } from 'vuex';
+import { createStore } from "vuex";
+import transactions from "@/services/transactions";
 
 export default createStore({
   state: {
+    idUser: "",
+    transactions: [],
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    newUser(state, idUser) {
+      state.idUser = idUser;
+    },
+    insertTransactions(state) {
+      transactions
+        .getTransactions(state.idUser)
+        .then((res) => {
+          state.transactions = res.data;
+        })
+        .catch(() => {
+          alert("Error!");
+        });
+    },
   },
-  actions: {
-  },
-  modules: {
-  },
+  actions: {},
+  modules: {},
 });
