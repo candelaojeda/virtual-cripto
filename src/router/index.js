e7ec404ffbf904ea3c21bd58e6df1ff5fa671dca
candelaojeda <ojedacandelaco@gmail.com>
2022-07-25 18:38:01 -0300

    added route transactions

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login.vue';
+import Transactions from "../views/Transactions.vue";
 
 const routes = [
   {
     path: '/',
     name: 'login',
     component: Login,
+  },
+  {
+    path: '/transactions',
+    name: 'Transactions',
+    component: Transactions,
   },
   {
     path: '/about',
