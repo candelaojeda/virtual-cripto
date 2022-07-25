8ae30ddccbd042eae85c87300476f0d6ed446654
candelaojeda <ojedacandelaco@gmail.com>
2022-07-25 18:36:58 -0300

    added router links

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,64 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Login</router-link> |
+    <router-link to="/transactions">Purchase-Sale</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view></router-view>
 </template>
-
+<script>
+  export default  {
+    data() {
+      return {};
+    }
+  }
+</script>
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap");
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Poppins;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
+.login {
+  position: absolute;
+  top: -5px;
+  left: -5px;
+  right: -10px;
+  bottom: -10px;
+  width: auto;
+  height: auto;
+  background-image: url("./assets/img-logo.avif");
+  background-size: cover;
+  z-index: 0;
+}
+.body {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  bottom: 0px;
+  width: auto;
+  height: auto;
+  background-image: url("./assets/img-logo.avif");
+  background-size: cover;
+  z-index: 0;
+}
 nav {
   padding: 30px;
 }
-
 nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 nav a.router-link-exact-active {
   color: #42b983;
 }
