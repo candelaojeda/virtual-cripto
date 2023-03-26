1d3a213961b57e0a003b25861f44f21cf7125132
candelaojeda <ojedacandelaco@gmail.com>
2023-03-26 13:30:45 -0300

    import investments and added path

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
 import Transactions from "../views/Transactions.vue";
 import History from "../views/History.vue";
+import Investments from "../views/Investments.vue";
 
 const routes = [
   {
@@ -25,7 +26,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: () => import(/* webpackChunkName: "about" */ "../views/ActualStatus.vue"),
+  },
+  {
+    path: "/investments",
+    name: "Investments",
+    component: Investments,
   },
 ];
 
